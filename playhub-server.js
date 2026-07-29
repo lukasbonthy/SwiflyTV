fb80c029caabc92e6c7f0fdb4d98c93c8f2681e2
lukasbonthy <126826970+lukasbonthy@users.noreply.github.com>
2026-07-29 02:45:14 -0400

    Allow dynamic game progress styles

--- playhub-server.js
+++ playhub-server.js
@@ -21,7 +21,7 @@ app.use(
       directives: {
         defaultSrc: ["'self'"],
         scriptSrc: ["'self'"],
-        styleSrc: ["'self'"],
+        styleSrc: ["'self'", "'unsafe-inline'"],
         imgSrc: ["'self'", "data:"],
         connectSrc: ["'self'"],
         fontSrc: ["'self'", "data:"],
